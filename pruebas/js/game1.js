3d3d6b31dce74a315a4b2f65ad901a146afe6b2c
Josue <josue.barrios@gmail.com>
2018-07-11 23:17:54 -0500

    tamaño y escalado

--- pruebas/js/game1.js
+++ pruebas/js/game1.js
@@ -4,8 +4,9 @@ let gameScene = new Phaser.Scene('Game');
 // Load assets
 gameScene.preload = function(){
     // load images
-    this.load.image('background', 'assets/road.png');
+    this.load.image('background', 'assets/background.png');
     this.load.image('player', 'assets/player.png');
+    this.load.image('enemy', 'assets/dragon.png');
 };
 
 // llamado despues del preload
@@ -19,8 +20,35 @@ gameScene.create = function(){
     let gameH = this.sys.game.config.height;
 
     bg.setPosition(gameW/2,gameH/2);
-    
+
+    // crear el jugador
+    this.jugador = this.add.sprite(60,180,'player');
+    this.jugador.setScale(0.5);
+
+
+    //crear un enemigo
+    this.enemy1 = this.add.sprite(250,180,'enemy');
+    this.enemy2 = this.add.sprite(450,180,'enemy');
+
+    // flip
+    this.enemy1.flipX =true;
+
+    this.altoPlayer = this.jugador.height;
+    this.anchoPlayer = this.jugador.width;
+
+    console.log(this.jugador);
+
 }
+
+// el update se realiza a 60 veces por segundo
+
+gameScene.update = function(){
+    
+    if (this.jugador.scaleX < 2){
+        this.jugador.setScale(this.jugador.scaleX + 0.01);
+    }
+}
+
 
 // configurar el juego
 
